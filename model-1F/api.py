42e1ca0b0b029038bd547faaf57021a4e8fdf76a
Isam <nfs.issamhajji@gmail.com>
2025-05-20 17:55:27 +0200

    Update api.py

--- model-1F/api.py
+++ model-1F/api.py
@@ -60,5 +60,6 @@ def process_image():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host='0.0.0.0', port=port)
     # app.run(debug=True)
